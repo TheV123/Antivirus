04bd6f702bb3bf90e82a4363a67997bf720af37c
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-05-16 12:01:50 -0400

    sha1file and md5file

--- directory_scanner.py
+++ directory_scanner.py
@@ -8,17 +8,27 @@ def sha256_file(filename):
     hash_value = hashlib.sha256(file_content)
     return hash_value.hexdigest()
 
+def sha1_file(filename):
+    with open(filename, 'rb') as f:
+        file_content = f.read()
+    hash_value = hashlib.sha1(file_content)
+    return hash_value.hexdigest()
+
+def md5_file(filename):
+    with open(filename, 'rb') as f:
+        file_content = f.read()
+    hash_value = hashlib.md5(file_content)
+    return hash_value.hexdigest()
+
 def scan_directory(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             filename = os.path.join(root, file)
-            print(f"Filename: {filename}, Hash: {sha256_file(filename)}")
+            print(f"Filename: {filename}, Sha256: {sha256_file(filename)}, Sha1: {sha1_file(filename)}, Md5 {md5_file(filename)}")
 
 def main():
     directory = os.getcwd()
     scan_directory(directory)
-    
-    
     
 if __name__ == '__main__':
     main()
