a98b05b60dc2b13180b1ff9d96d802b19c691f9f
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-05-16 12:47:16 -0400

    sha1hashes txt file and sha1scan

--- directory_scanner.py
+++ directory_scanner.py
@@ -3,13 +3,8 @@ import hashlib
 import sys
 
 md5_malware_hashes = []
+sha1_malware_hashes = []
 sha256_malware_hashes = []
-
-def sha256_file(filename):
-    with open(filename, 'rb') as f:
-        file_content = f.read()
-    hash_value = hashlib.sha256(file_content)
-    return hash_value.hexdigest()
 
 def sha1_file(filename):
     with open(filename, 'rb') as f:
@@ -17,11 +12,23 @@ def sha1_file(filename):
     hash_value = hashlib.sha1(file_content)
     return hash_value.hexdigest()
 
+def sha256_file(filename):
+    with open(filename, 'rb') as f:
+        file_content = f.read()
+    hash_value = hashlib.sha256(file_content)
+    return hash_value.hexdigest()
+
 def md5_file(filename):
     with open(filename, 'rb') as f:
         file_content = f.read()
     hash_value = hashlib.md5(file_content)
     return hash_value.hexdigest()
+
+def sha1_scan(filename):
+    with open(filename, 'r') as f:
+        global sha1_malware_hashes
+        if sha1_file(filename) in sha1_malware_hashes:
+            print("SHA1 hash matches a known malware hash!")
 
 def sha256_scan(filename):
     with open(filename, 'r') as f:
@@ -43,15 +50,21 @@ def scan_directory(directory):
             filename = os.path.join(root, file)
             md5_scan(filename)
             sha256_scan(filename)
+            sha1_scan(filename)
 
 def main():
     global md5_malware_hashes
     #gotten from https://github.com/Len-Stevens/MD5-Malware-Hashes/tree/main
     with open("MD5_hashes.txt", "r") as f:
         md5_malware_hashes = f.read().splitlines()
-            
+    
+    #gotten from MalwareBazaar https://bazaar.abuse.ch/export/
+    global sha1_malware_hashes
+    with open("full_sha1.txt", "r") as f:
+        sha1_malware_hashes = f.read().splitlines()[9:-1]
+    
     global sha256_malware_hashes
-    #gotten from MalwareBazaar https://bazaar.abuse.ch/export/#csv
+    #gotten from MalwareBazaar https://bazaar.abuse.ch/export/
     with open("full_sha256.txt", "r") as f:
         sha256_malware_hashes = f.read().splitlines()[9:-1]
     
