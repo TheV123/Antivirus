ab2a2628e54b7da34a8369a33000ea6f60afab4b
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-05-17 19:11:25 -0400

    setup_argument_parser

--- args.py
+++ args.py
@@ -2,3 +2,26 @@ import argparse, os
 
 
 DESCRIPTION = "Simple Malware Scanner"
+
+def setup_argument_parser() -> argparse.ArgumentParser:
+    os.environ.setdefault("COLUMNS", "100")
+    
+    parser = argparse.ArgumentParser(
+        prog="malware_scanner",
+        description=DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-scd",
+        "--scan_current_directory",
+        help="Scan the current directory the program is in",
+    )
+    parser.add_argument(
+        "-s",
+        "--scan_directory",
+        help="Scan a directory with a path provided as an argument",
+    )
+
+def parse_args(argv = None) -> argparse.Namespace:
+    parser = setup_argument_parser()
+    args = parse_args(argv=argv)
