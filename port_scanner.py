df980e5e394791814ca91a435ea053c45d77575d
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-05-19 19:52:53 -0400

    basic port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -1,2 +1,29 @@
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+
 def scan_ports():
-    pass
+    subprocess.call('clear', shell=True)
+    
+    START_PORT = 1
+    END_PORT = 65535    
+    # print("hello")
+    for port in range(START_PORT, END_PORT):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.01)
+            result = sock.connect_ex(('localhost', port))
+            if result == 0:
+                service = socket.getservbyport(port)
+                print(f"Your computer is connected to port {port} with service {service}")
+            sock.close()
+        except KeyboardInterrupt:
+            print("Exiting...")
+            sys.exit()
+        except socket.error:
+            print(f"Couldn't connect to server {port}")
+            sys.exit()
+
+scan_ports()
